ed0a02e5f10870677d81d126dd55eccaf6f174af
Kumpis Audrius <audrius.kumpis@mif.stud.vu.lt>
2022-05-12 20:35:16 +0300

    updated logging

--- src/main/java/com/audriuskumpis/interceptors/MethodLogger.java
+++ src/main/java/com/audriuskumpis/interceptors/MethodLogger.java
@@ -11,7 +11,9 @@ public class MethodLogger implements Serializable {
 
     @AroundInvoke
     public Object performLogging(InvocationContext context) throws Exception {
+        System.out.println("===========================");
         System.out.println("Called method: " + context.getMethod().getDeclaringClass().getSimpleName() + "." + context.getMethod().getName() + "()");
+        System.out.println("===========================");
         return context.proceed();
     }
 }
